492d6168d72f207ddc88c967b260269f4797c63a
miditerminali <topbas_melih_70_70@hotmail.com>
2023-02-23 18:30:35 +0300

    23.02.2023

---
+++ Warehouse.Service/Admin/CountryService.cs
@@ -0,0 +1,53 @@
+﻿using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Data;
+using Warehouse.ViewModels.Admin;
+
+namespace Warehouse.Service.Admin
+{
+    public class CountryService
+    {
+        private readonly WarehouseManagementSystemEntities1 _context;
+
+        public CountryService(WarehouseManagementSystemEntities1 context)
+        {
+            _context = context; 
+        }
+        private IQueryable<CountryListViewModel> _getCountryListIQueryable(Expression<Func<Data.Countries, bool>> expr)
+        {
+            return (from b in _context.Countries.AsExpandable().Where(expr)
+                    select new CountryListViewModel()
+                    {
+                        Id= b.Id,
+                        Name = b.Name
+
+                    });
+        }
+
+        public IQueryable<CountryListViewModel> GetCountryListIQueryable(OrderSearchViewModel model)
+        {
+            var predicate = PredicateBuilder.New<Data.Countries>(true);/*AND*/ 
+            if (!string.IsNullOrWhiteSpace(model.SearchName))
+            {
+                predicate.And(a => a.Name.Contains(model.SearchName));
+
+            }
+            return _getCountryListIQueryable(predicate);
+        }
+
+        public async Task<CountryListViewModel> GetServiceListViewAsync(long countryId)
+        {
+
+            var predicate = PredicateBuilder.New<Data.Countries>(true);/*AND*/
+            predicate.And(a => a.Id == countryId);
+            var country = await _getCountryListIQueryable(predicate).SingleOrDefaultAsync().ConfigureAwait(false);
+            return country;
+        }
+    }
+}
---
+++ WarehouseManagementSystem/Areas/Admin/Controllers/CountrySettingController.cs
@@ -0,0 +1,58 @@
+﻿using DocumentFormat.OpenXml.EMMA;
+using Microsoft.Web.Mvc;
+using MvcPaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Warehouse.Service.Admin;
+using Warehouse.Utils.Constants;
+using Warehouse.ViewModels.Admin;
+
+namespace WarehouseManagementSystem.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class CountrySettingController : AdminBaseController
+    {
+        private readonly CountryService _countryService;
+        private readonly LanguageService _languageService;
+        public CountrySettingController(LanguageService languageService, CountryService countryService)
+        {
+            _languageService = languageService;
+            _countryService = countryService;
+        }
+        // GET: Admin/CountrySetting
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Title = "Servisler";
+            ViewBag.Languages = await _languageService.GetLanguageListViewAsync();
+            return View();
+        }
+        [AjaxOnly, HttpPost, ValidateInput(false)]
+        public async Task<ActionResult> CountryList(OrderSearchViewModel searchViewModel, int? page)
+        {
+            var currentPageIndex = page - 1 ?? 0;
+
+            var result = _countryService.GetCountryListIQueryable(searchViewModel)
+                .OrderBy(p => p.Name)
+                .ToPagedList(currentPageIndex, SystemConstants.DefaultServicePageSize);
+
+            ViewBag.Languages = await _languageService.GetLanguageListViewAsync();
+            ModelState.Clear();
+
+            ViewBag.LanguageId = searchViewModel.LanguageId;
+            return new ContentResult
+            {
+                ContentType = "application/json",
+                Content = new JavaScriptSerializer { MaxJsonLength = Int32.MaxValue }.Serialize(new
+                {
+                    success = true,
+                    responseText = RenderPartialViewToString("~/Areas/Admin/Views/CountrySetting/CountryList.cshtml", result)
+                })
+            };
+        }
+    }
+}
