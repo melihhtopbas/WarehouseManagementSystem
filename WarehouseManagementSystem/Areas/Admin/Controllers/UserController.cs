64d12173d20d34659baa4f6305ff7978598ebaa6
melihhtopbas <topbas_melih_70@hotmail.com>
2023-03-01 22:48:31 +0300

    01.03.2023

---
+++ WarehouseManagementSystem/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml.EMMA;
+using Microsoft.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WarehouseManagementSystem.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+        [HttpGet,AjaxOnly]
+        public ActionResult ChangePassword()
+        {
+            return PartialView("~/Areas/Admin/Views/User/ChangePassword.cshtml");
+        }
+    }
+}
